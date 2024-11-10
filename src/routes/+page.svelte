5ed57c77663fb540cc2ad3b0e070751776382876
edo1z <endo.yuta@logicky.com>
2024-11-11 04:25:54 +0900

    ラグメニュー追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,36 @@
-<ul>
-  <li><a href="/collections">Collections</a></li>
-</ul>
+<nav class="p-8">
+  <ul class="flex flex-col gap-4">
+    <li>
+      <a
+        href="/collections"
+        class="
+          block p-4 w-64
+          bg-gradient-to-r from-purple-500 to-pink-500
+          text-white font-bold
+          rounded-lg shadow-lg
+          transform transition duration-300
+          hover:scale-105 hover:shadow-xl
+          hover:from-purple-600 hover:to-pink-600
+        "
+      >
+        Collections
+      </a>
+    </li>
+    <li>
+      <a
+        href="/rag"
+        class="
+          block p-4 w-64
+          bg-gradient-to-r from-blue-500 to-cyan-500
+          text-white font-bold
+          rounded-lg shadow-lg
+          transform transition duration-300
+          hover:scale-105 hover:shadow-xl
+          hover:from-blue-600 hover:to-cyan-600
+        "
+      >
+        RAG
+      </a>
+    </li>
+  </ul>
+</nav>
