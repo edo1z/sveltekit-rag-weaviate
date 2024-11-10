773408e5386e2872b387abb43a304826974ac43b
edo1z <endo.yuta@logicky.com>
2024-11-10 16:44:48 +0900

    ナブバーのリンク

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
 	let { children } = $props();
 </script>
 
-<div class='p-3 bg-gray-300'>Rag-1</div>
+<div class='p-3 bg-gray-300'>
+  <a href="/" class="hover:text-gray-600">Rag-1</a>
+</div>
 <div class='p-6'>
 {@render children()}
 </div>
