0d51e3581d45839ce68a670b3c4438fd08a1a752
edo1z <endo.yuta@logicky.com>
2024-11-11 04:38:38 +0900

    ragテーブル追加

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -13,6 +13,19 @@ export const session = sqliteTable("session", {
     expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 });
 
+export const rag = sqliteTable('rag', {
+  id: text('id').primaryKey(),
+  userId: text('user_id').notNull().references(() => user.id),
+  name: text('name').notNull(),
+  description: text('description'),
+  promptForQuery: text('prompt_for_query').notNull(),
+  promptForResult: text('prompt_for_result').notNull(),
+  createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+  updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull()
+});
+
 export type Session = typeof session.$inferSelect;
 
 export type User = typeof user.$inferSelect;
+
+export type Rag = typeof rag.$inferSelect;
