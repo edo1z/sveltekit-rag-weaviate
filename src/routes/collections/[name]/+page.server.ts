4454fdd54ea080c0cdde72ee264eac3b08de75cb
edo1z <endo.yuta@logicky.com>
2024-11-10 16:52:02 +0900

    コレクションのデータを表示

--- src/routes/collections/[name]/+page.server.ts
+++ src/routes/collections/[name]/+page.server.ts
@@ -1,7 +1,33 @@
+import weaviate from 'weaviate-client';
+import { env } from '$env/dynamic/private';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
+  const client = await weaviate.connectToWeaviateCloud(
+    env.WEAVIATE_URL,
+    {
+      authCredentials: new weaviate.ApiKey(env.WEAVIATE_API_KEY),
+      headers: {
+        'X-OpenAI-Api-Key': env.OPENAI_API_KEY,
+      },
+    }
+  );
+
+  const collection = client.collections.get(params.name);
+  const items = [];
+
+  for await (let item of collection.iterator({
+    includeVector: true
+  })) {
+    items.push({
+      uuid: item.uuid,
+      properties: item.properties,
+      vectors: item.vectors
+    });
+  }
+
   return {
-    name: params.name
+    name: params.name,
+    items
   };
 };
